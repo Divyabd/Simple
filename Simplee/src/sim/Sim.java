0e4a510bc27bfdd1e621f37d96bb7075593479f0
Divyabd <divyagowda8393@gmail.com>
2021-02-08 11:05:56 +0530

    1st commit

---
+++ Simplee/src/sim/Sim.java
@@ -0,0 +1,13 @@
+package sim;
+
+public class Sim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+	
+		System.out.println("This is divya");
+	}
+
+}
